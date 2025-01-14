3f351a86e71084d27e9361b0f85cc1cbbc902d1c
Tishues <BradleyBounds@outlook.com>
2025-01-14 09:04:41 -0600

    B: added tickrate 60fps

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from constants import *
 
 def main():
         pygame.init
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        clock = pygame.time.Clock()
+        dt = 0
 
         print("Starting asteroids!")
         print(f"Screen width: {SCREEN_WIDTH}")
         print(f"Screen height: {SCREEN_HEIGHT}")
 
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         while True:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
-                        return pygame.Surface.fill(screen, (0,0,0))
-              
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        return pygame.display.flip(screen)
+                        return 
+                pygame.Surface.fill(screen, (0,0,0))
+                pygame.display.flip()
+                dt = clock.tick(60) / 1000
               
 
 if __name__ == "__main__":
